ec7a5b3802cb86ec02526d0fed2197b0c45a1289
Adam Preston <adam.m.preston@gmail.com>
2018-03-23 16:03:11 -0600

    add lambda function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # forecastalerter-lambda
 The AWS Lambda function for handling the managing the email list
+
+### Deploy this to AWS
+`aws lambda update-function-code --function-name subscribeUserFunction --zip-file fileb:///PATH/TO/lambdaFunc.zip`
---
+++ index.js
@@ -0,0 +1,48 @@
+exports.handler = (event, context, callback) => {
+
+    var request = require('request');
+    
+    var sender = event.Records[0].ses.mail.source;
+    var subject = event.Records[0].ses.mail.commonHeaders.subject; 
+
+
+    if (subject.toLowerCase() == 'subscribe'){
+
+    	var formData = {
+		  email: sender
+		};
+
+    	request.post({url:'https://forecastalerter.herokuapp.com/api/register', 
+		formData: formData}, function(error,httpResponse,body){ 
+
+			if (!error && httpResponse.statusCode == 201) {
+	    		console.log('account registered: ' + sender); 
+	  		} else {
+	  			console.log('nothing happend because of this error code: ' + httpResponse.statusCode ); 
+	  		}
+
+		});
+
+    } else if (subject.toLowerCase() == 'unsubscribe'){
+
+    	var formData = {
+		  email: sender
+		};
+
+    	request.post({url:'https://forecastalerter.herokuapp.com/api/unregister', 
+		formData: formData}, function(error,httpResponse,body){ 
+
+			if (!error && httpResponse.statusCode == 204) {
+	    		console.log('account unregistered:' + sender); 
+	  		} else {
+	  			console.log('error code' + httpResponse && httpResponse.statusCode); 
+	  		}
+
+		});
+
+    } else {
+    	console.log('unsupported function:' + subject); 
+    }
+ 
+    callback(null);
+};
